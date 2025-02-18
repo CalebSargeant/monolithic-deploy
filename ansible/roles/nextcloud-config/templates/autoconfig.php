bc146f8f09486b15fd157c62d14e7f62c47c17e8
Caleb Sargeant <156674873+caleb-sargeant-bt@users.noreply.github.com>
2025-02-18 12:51:58 +0100

    Add Nextcloud and PostgreSQL configuration automation
    Introduces Ansible roles for automating Nextcloud and PostgreSQL setup, including database creation, user management, and configuration adjustments. Updates Nextcloud deployment with trusted domain settings and rewrites Nginx to listen on port 8080. This improves deployment consistency and simplifies the setup process.

---
+++ ansible/roles/nextcloud-config/templates/autoconfig.php
@@ -0,0 +1,9 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "pgsql",
+  "dbname"        => "nextcloud",
+  "dbuser"        => "admin",
+  "dbpass"        => "admin",
+  "dbhost"        => "postgres.database.svc.cluster.local",
+  "dbtableprefix" => "",
+);
